f4a425ffa27997c91df607547c92a6468deac022
garamond13 <98652255+garamond13@users.noreply.github.com>
2023-07-01 13:05:55 +0200

    make ImVec2& operator+= noexcept

--- w-image-viewer/wiv_imconfig.h
+++ w-image-viewer/wiv_imconfig.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define IM_VEC2_CLASS_EXTRA  \
-	constexpr ImVec2& operator+=(const ImVec2& other) \
+	constexpr ImVec2& operator+=(const ImVec2& other) noexcept \
 	{ \
 		x += other.x; \
 		y += other.y; \
