8e862091e96ef0f9e065532b69b55f81db9dc5b6
garamond13 <98652255+garamond13@users.noreply.github.com>
2023-07-01 13:55:34 +0200

    use imguis math operators
    use imguis math operators instead of implementing our own

--- w-image-viewer/wiv_imconfig.h
+++ w-image-viewer/wiv_imconfig.h
@@ -1,9 +1,3 @@
 #pragma once
 
-#define IM_VEC2_CLASS_EXTRA  \
-	constexpr ImVec2& operator+=(const ImVec2& other) noexcept \
-	{ \
-		x += other.x; \
-		y += other.y; \
-		return *this; \
-	}
+#define IMGUI_DEFINE_MATH_OPERATORS
