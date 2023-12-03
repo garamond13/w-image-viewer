620849e2084b54d0db5d3ad34a7c9ff89d0f4a6b
garamond13 <98652255+garamond13@users.noreply.github.com>
2023-12-03 02:59:09 +0100

    edit ps_cms

--- w-image-viewer/ps_cms.hlsl
+++ w-image-viewer/ps_cms.hlsl
@@ -12,41 +12,41 @@ SamplerState smp : register(s0);
 
 float4 main(Vs_out vs_out) : SV_Target
 {
-    float4 color = tex.SampleLevel(smp, vs_out.texcoord, 0.0); 
-    float3 index = saturate(color.rgb) * (LUT_SIZE - 1.0);
+    const float4 color = tex.SampleLevel(smp, vs_out.texcoord, 0.0); 
+    const float3 index = saturate(color.rgb) * (LUT_SIZE - 1.0);
     
     // Get barycentric weights.
     // See https://doi.org/10.2312/egp.20211031
     //
     
-    float3 r = frac(index);
-    float3 vert2 = 0.0;
-    float3 vert3 = 1.0;
-    bool3 c = r.xyz >= r.yzx;
-    bool c_xy = c.x;
-    bool c_yz = c.y;
-    bool c_zx = c.z;
-    bool c_yx =!c.x;
-    bool c_zy =!c.y;
-    bool c_xz =!c.z;
+    const float3 r = frac(index);
+    const bool3 c = r.xyz >= r.yzx;
+    const bool c_xy = c.x;
+    const bool c_yz = c.y;
+    const bool c_zx = c.z;
+    const bool c_yx =!c.x;
+    const bool c_zy =!c.y;
+    const bool c_xz =!c.z;
     bool cond;
     float3 s = 0.0;
+    float3 vert2 = 0.0;
+    float3 vert3 = 1.0;
             
-#define ORDER(x,y,z) \
+#define order(x,y,z) \
     cond = c_ ## x ## y && c_ ## y ## z; \
     s = cond ? r.x ## y ## z : s; \
     vert2.x = cond ? 1.0 : vert2.x; \
     vert3.z = cond ? 0.0 : vert3.z;
     
-    ORDER(x, y, z)
-    ORDER(x, z, y)
-    ORDER(z, x, y)
-    ORDER(z, y, x)
-    ORDER(y, z, x)
-    ORDER(y, x, z)
+    order(x, y, z)
+    order(x, z, y)
+    order(z, x, y)
+    order(z, y, x)
+    order(y, z, x)
+    order(y, x, z)
     
     //
     
-    float3 base = floor(index) + 0.5;
+    const float3 base = floor(index) + 0.5;
     return float4(lut.SampleLevel(smp, base / LUT_SIZE, 0.0).rgb * (1.0 - s.x) + lut.SampleLevel(smp, (base + 1.0) / LUT_SIZE, 0.0).rgb * s.z + lut.SampleLevel(smp, (base + vert2) / LUT_SIZE, 0.0).rgb * (s.x - s.y) + lut.SampleLevel(smp, (base + vert3) / LUT_SIZE, 0.0).rgb * (s.y - s.z), color.a);
 }
