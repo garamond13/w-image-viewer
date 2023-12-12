dc5ded5a9db4a65f4e9f90d9ca248a411a53bd3c
garamond13 <98652255+garamond13@users.noreply.github.com>
2023-12-12 01:12:32 +0100

    extend supported extensions

--- w-image-viewer/supported_extensions.h
+++ w-image-viewer/supported_extensions.h
@@ -1,15 +1,16 @@
 #pragma once
 
 inline constexpr auto WIV_SUPPORTED_EXTENSIONS{ L""
-	"*.tif;*.tiff;*.tx;*.env;*.sm;*.vsm;" //tagged image file format (tiff) 
-	"*.jpg;*jpeg;*.jpe;*.jif;*.jfif;*.jfi;" //joint photographic experts group (jpeg)
-	"*.png;" //portable network graphics (png)
-	"*.bmp;*.rle;*.dib;" //bitmap image (bmp)
-	"*.exr;*.sxr;*.mxr;" //openEXR
-	"*.pbm;*.pgm;*.ppm;*.pnm;" //pnm/netpbm
-	"*.psd;*.pdd;*.psb;" //psd
-	"*.tga;*.icb;*.vda;*.vst;*.tpic;" //truevision tga (targa)
-	"*.ico;" //icon
-	"*.bay;*.bmq;*.cr2;*.cr3;*.crw;*.cs1;*.dc2;*.dcr;*.dng;*.erf;*.fff;*.hdr;*.k25;*.kdc;*.mdc;*.mos;*.mrw;*.nef;*.orf;*.pef;*.pxn;*.raf;*.raw;*.rdc;*.sr2;*.srf;*.arw;*.3fr;*.cine;*.ia;*.kc2;*.mef;*.nrw;*.qtk;*.rw2;*.sti;*.rwl;*.srw;*.drf;*.dsc;*.ptx;*.cap;*.iiq;*.rwz;" //raw
-	"*.webp;" //webp
+	"*.tif;*.tiff;*.tx;*.env;*.sm;*.vsm;" // Tagged Image File Format (TIFF) 
+	"*.jpg;*jpeg;*.jpe;*.jif;*.jfif;*.jfi;" // Joint Photographic Experts group (JPEG)
+	"*.jp2;*.j2k;" // JPEG-2000
+	"*.png;" // Portable Network Graphics (png)
+	"*.bmp;*.rle;*.dib;" // Bitmap Image (bmp)
+	"*.exr;*.sxr;*.mxr;" // OpenEXR
+	"*.pbm;*.pgm;*.ppm;*.pnm;" // PNM / Netpbm
+	"*.psd;*.pdd;*.psb;" // PSD
+	"*.tga;*.icb;*.vda;*.vst;*.tpic;" // Truevision TGA (targa)
+	"*.ico;" // Icon
+	"*.bay;*.bmq;*.cr2;*.cr3;*.crw;*.cs1;*.dc2;*.dcr;*.dng;*.erf;*.fff;*.hdr;*.k25;*.kdc;*.mdc;*.mos;*.mrw;*.nef;*.orf;*.pef;*.pxn;*.raf;*.raw;*.rdc;*.sr2;*.srf;*.x3f;*.arw;*.3fr;*.cine;*.ia;*.kc2;*.mef;*.nrw;*.qtk;*.rw2;*.sti;*.rwl;*.srw;*.drf;*.dsc;*.ptx;*.cap;*.iiq;*.rwz;" // RAW
+	"*.webp;" // WEBP
 };
