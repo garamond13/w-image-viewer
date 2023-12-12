bfe4a90407700a6d35227b936ce388078ff2e484
garamond13 <98652255+garamond13@users.noreply.github.com>
2023-12-12 02:32:08 +0100

    update bench
    Add blocking bench end function versions.

--- w-image-viewer/bench.h
+++ w-image-viewer/bench.h
@@ -3,10 +3,9 @@
 #include "pch.h"
 
 // Helpers for benching execution time.
-// First call bench_start() than bench_end().
+// bench_start() must be called first!
 
 // DON'T USE THIS DIRECTLY!
-// It should be optimized out in release build.
 inline std::chrono::high_resolution_clock::time_point bench_start_time;
 
 inline void bench_start() noexcept
@@ -14,6 +13,7 @@ inline void bench_start() noexcept
 	bench_start_time = std::chrono::high_resolution_clock::now();
 }
 
+// Non blocking.
 // The result will be in milliseconds (double).
 inline void bench_end() noexcept
 {
@@ -22,10 +22,25 @@ inline void bench_end() noexcept
 	t.detach();
 }
 
+// Non blocking.
 // The result will be in milliseconds (double).
 inline void bench_end(const std::wstring what) noexcept
 {
 	const auto bench_end_time{ std::chrono::high_resolution_clock::now() };
 	std::thread t([=]() noexcept { MessageBoxW(nullptr, std::to_wstring(std::chrono::duration<double, std::chrono::milliseconds::period>(bench_end_time - bench_start_time).count()).c_str(), what.c_str(), 0); });
 	t.detach();
+}
+
+// The result will be in milliseconds (double).
+inline void bench_end_blocking() noexcept
+{
+	const auto bench_end_time{ std::chrono::high_resolution_clock::now() };
+	MessageBoxW(nullptr, std::to_wstring(std::chrono::duration<double, std::chrono::milliseconds::period>(bench_end_time - bench_start_time).count()).c_str(), L"Bench result", 0);
+}
+
+// The result will be in milliseconds (double).
+inline void bench_end_blocking(const std::wstring what) noexcept
+{
+	const auto bench_end_time{ std::chrono::high_resolution_clock::now() };
+	MessageBoxW(nullptr, std::to_wstring(std::chrono::duration<double, std::chrono::milliseconds::period>(bench_end_time - bench_start_time).count()).c_str(), what.c_str(), 0);
 }
