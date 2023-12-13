cfcc802176f9147ed18153a7c295481f31f0b68d
garamond13 <98652255+garamond13@users.noreply.github.com>
2023-12-13 16:23:37 +0100

    shader_config: edit comments

--- w-image-viewer/shader_config.h
+++ w-image-viewer/shader_config.h
@@ -1,14 +1,14 @@
 #ifndef __SHADER_CONFIG_H__
 #define __SHADER_CONFIG_H__
 
-//this file is shared by both hlsl and cpp
+// This file is shared by both hlsl and cpp.
 
-//enum WIV_CMS_TRC_
+// enum WIV_CMS_TRC_
 #define WIV_CMS_TRC_NONE 0
 #define WIV_CMS_TRC_GAMMA 1
 #define WIV_CMS_TRC_SRGB 2
 
-//enum WIV_KERNEL_FUNCTION_
+// enum WIV_KERNEL_FUNCTION_
 #define WIV_KERNEL_FUNCTION_LANCZOS 0
 #define WIV_KERNEL_FUNCTION_GINSENG 1
 #define WIV_KERNEL_FUNCTION_HAMMING 2
