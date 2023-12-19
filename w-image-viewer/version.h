869d630f90748ce608fe54367a3fbc792ae29211
garamond13 <98652255+garamond13@users.noreply.github.com>
2023-12-19 18:39:52 +0100

    bump version number before release
    - 1.0.2

--- w-image-viewer/version.h
+++ w-image-viewer/version.h
@@ -2,4 +2,4 @@
 
 inline constexpr int WIV_VERSION_NUMBER_MAJOR{ 1 };
 inline constexpr int WIV_VERSION_NUMBER_MINOR{ 0 };
-inline constexpr int WIV_VERSION_NUMBER_PATCH{ 1 };
+inline constexpr int WIV_VERSION_NUMBER_PATCH{ 2 };
