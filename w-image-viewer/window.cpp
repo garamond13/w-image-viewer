f3f18eedd5c852bd3c32016e97095d9b01ea8337
garamond13 <98652255+garamond13@users.noreply.github.com>
2024-07-04 00:33:27 +0200

    Edit window procedure

--- w-image-viewer/window.cpp
+++ w-image-viewer/window.cpp
@@ -16,10 +16,8 @@ namespace
     constexpr auto WIV_WINDOW_NAME = L"W Image Viewer";
 }
 
-// Forward declare message handler from imgui_impl_win32.cpp.
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
-Window::Window(HINSTANCE hinstance, int ncmdshow)
+Window::Window(HINSTANCE hinstance, int ncmdshow) :
+    is_minimized(false)
 {
     // Register window class.
     const WNDCLASSEXW wndclassexw = {
@@ -38,7 +36,11 @@ Window::Window(HINSTANCE hinstance, int ncmdshow)
         .bottom = g_config.window_height.val
     };
     wiv_assert(AdjustWindowRectEx(&rect, WIV_WINDOW_STYLE, FALSE, WIV_WINDOW_EX_STYLE), != 0);
-    g_hwnd = CreateWindowExW(WIV_WINDOW_EX_STYLE, wndclassexw.lpszClassName, WIV_WINDOW_NAME, WIV_WINDOW_STYLE, CW_USEDEFAULT, CW_USEDEFAULT, rc_w<int>(rect), rc_h<int>(rect), nullptr, nullptr, hinstance, this);
+    g_hwnd = CreateWindowExW(WIV_WINDOW_EX_STYLE, wndclassexw.lpszClassName, WIV_WINDOW_NAME, WIV_WINDOW_STYLE, CW_USEDEFAULT, CW_USEDEFAULT, rc_w<int>(rect), rc_h<int>(rect), nullptr, nullptr, hinstance, nullptr);
+    assert(g_hwnd);
+
+    // Set "this" pointer.
+    SetWindowLongPtrW(g_hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
 
     renderer.create();
     ShowWindow(g_hwnd, ncmdshow);
@@ -65,23 +67,20 @@ int Window::message_loop()
     return static_cast<int>(msg.wParam);
 }
 
+// Forward declare message handler from imgui_impl_win32.cpp.
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 /* static */ LRESULT Window::wndproc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 {
+    // Get "this" pointer.
+    auto* this_ptr = reinterpret_cast<Window*>(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
+    if (!this_ptr)
+        return DefWindowProcW(hwnd, message, wparam, lparam);
+
     if (ImGui_ImplWin32_WndProcHandler(hwnd, message, wparam, lparam))
         return 1;
 
-    // Get "this" pointer that we passed to CreateWindowExW().
-    auto* window = reinterpret_cast<Window*>(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
-
     switch (message) {
-
-        // WM_NCCREATE is not guarantied to be the first message.
-        [[unlikely]] case WM_NCCREATE:
-            window = reinterpret_cast<Window*>(reinterpret_cast<CREATESTRUCT*>(lparam)->lpCreateParams);
-            g_hwnd = hwnd;
-            SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(window));
-            break;
-
         case WM_ERASEBKGND:
             return 1;
 
@@ -108,16 +107,16 @@ int Window::message_loop()
             return 0;
 
         case WIV_WM_OPEN_FILE:
-            window->renderer.create_image();
+            this_ptr->renderer.create_image();
             if (g_config.window_autowh.val)
-                window->renderer.ui.auto_window_size();
-            window->renderer.ui.reset_image_panzoom();
-            window->reset_image_rotation();
-            window->set_window_name();
-            window->renderer.should_update = true;
+                this_ptr->renderer.ui.auto_window_size();
+            this_ptr->renderer.ui.reset_image_panzoom();
+            this_ptr->reset_image_rotation();
+            this_ptr->set_window_name();
+            this_ptr->renderer.should_update = true;
             return 0;
         case WM_SIZING:
-            if (g_config.window_keep_aspect.val && window->renderer.ui.file_manager.image.is_valid()) {
+            if (g_config.window_keep_aspect.val && this_ptr->renderer.ui.file_manager.image.is_valid()) {
                 auto rect = reinterpret_cast<RECT*>(lparam);
 
                 // Get client area.
@@ -126,7 +125,7 @@ int Window::message_loop()
                 const auto client_width = rc_w<double>(unadjusted_rect);
                 const auto client_height = rc_h<double>(unadjusted_rect);
 
-                const auto image_aspect = window->renderer.ui.file_manager.image.get_aspect<double>();
+                const auto image_aspect = this_ptr->renderer.ui.file_manager.image.get_aspect<double>();
                 const auto new_client_width = std::lround(client_height * image_aspect - client_width);
                 const auto new_client_height = std::lround(client_width / image_aspect - client_height);
                 switch (wparam) {
@@ -148,26 +147,26 @@ int Window::message_loop()
             }
             break;
         case WM_SIZE:
-            window->is_minimized = wparam == SIZE_MINIMIZED;
-            if (!window->is_minimized) {
-                window->renderer.on_window_resize();
-                window->renderer.should_update = true;
+            this_ptr->is_minimized = wparam == SIZE_MINIMIZED;
+            if (!this_ptr->is_minimized) {
+                this_ptr->renderer.on_window_resize();
+                this_ptr->renderer.should_update = true;
             }
             return 0;
         case WM_DROPFILES:
             wiv_assert(SetForegroundWindow(hwnd), != 0);
-            if (window->renderer.ui.file_manager.drag_and_drop(reinterpret_cast<HDROP>(wparam))) {
-                window->renderer.create_image();
+            if (this_ptr->renderer.ui.file_manager.drag_and_drop(reinterpret_cast<HDROP>(wparam))) {
+                this_ptr->renderer.create_image();
                 if (g_config.window_autowh.val)
-                    window->renderer.ui.auto_window_size();
-                window->renderer.ui.reset_image_panzoom();
-                window->reset_image_rotation();
-                window->set_window_name();
-                window->renderer.should_update = true;
+                    this_ptr->renderer.ui.auto_window_size();
+                this_ptr->renderer.ui.reset_image_panzoom();
+                this_ptr->reset_image_rotation();
+                this_ptr->set_window_name();
+                this_ptr->renderer.should_update = true;
             }
             return 0;
         case WIV_WM_RESET_RESOURCES:
-            window->renderer.reset_resources();
+            this_ptr->renderer.reset_resources();
             return 0;
         case WM_DESTROY:
             PostQuitMessage(0);
