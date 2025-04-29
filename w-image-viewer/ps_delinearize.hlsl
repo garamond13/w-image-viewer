38724ac2fc84d90fcd12398b38c3982fbe31d73d
garamond13 <98652255+garamond13@users.noreply.github.com>
2025-04-29 11:59:48 +0200

    Fix srgb linearization and delinearization

--- w-image-viewer/ps_delinearize.hlsl
+++ w-image-viewer/ps_delinearize.hlsl
@@ -12,9 +12,20 @@ cbuffer cb0 : register(b0)
     float rcp_gamma; // y
 }
 
-#define gamma(rgb) (pow((rgb), rcp_gamma))
+float3 linear_to_gamma(float3 rgb)
+{
+    return pow(rgb, rcp_gamma);
+}
 
-#define srgb(rgb) ((rgb) < 0.0031308 ? 12.92 * (rgb) : 1.055 * pow((rgb), 1.0 / 2.4) - 0.055)
+float linear_to_srgb(float x)
+{
+    return x < 0.0031308 ? 12.92 * x : 1.055 * pow(x, 1.0 / 2.4) - 0.055;
+}
+
+float3 linear_to_srgb(float3 rgb)
+{
+    return float3(linear_to_srgb(rgb.r), linear_to_srgb(rgb.g), linear_to_srgb(rgb.b));
+}
 
 float4 main(Vs_out vs_out) : SV_Target
 {
@@ -22,9 +33,9 @@ float4 main(Vs_out vs_out) : SV_Target
     
     [forcecase] switch (index) {
         case WIV_CMS_TRC_GAMMA:
-            return float4(gamma(color.rgb), color.a);
+            return float4(linear_to_gamma(color.rgb), color.a);
         case WIV_CMS_TRC_SRGB:
-            return float4(srgb(color.rgb), color.a);
+            return float4(linear_to_srgb(color.rgb), color.a);
     
         // Red image.
         default:
--- w-image-viewer/ps_linearize.hlsl
+++ w-image-viewer/ps_linearize.hlsl
@@ -12,9 +12,20 @@ cbuffer cb0 : register(b0)
     float gamma_value; // y
 }
 
-#define gamma(rgb) (pow((rgb), gamma_value))
+float3 gamma_to_linear(float3 rgb)
+{
+    return pow(rgb, gamma_value);
+}
 
-#define srgb(rgb) ((rgb) < 0.04045 ? (rgb) / 12.92 : pow(((rgb) + 0.055) / 1.055, 2.4))
+float srgb_to_linear(float x)
+{
+    return x < 0.04045 ? x / 12.92 : pow((x + 0.055) / 1.055, 2.4);
+}
+
+float3 srgb_to_linear(float3 rgb)
+{
+    return float3(srgb_to_linear(rgb.r), srgb_to_linear(rgb.g), srgb_to_linear(rgb.b));
+}
 
 float4 main(Vs_out vs_out) : SV_Target
 {
@@ -22,9 +33,9 @@ float4 main(Vs_out vs_out) : SV_Target
     
     [forcecase] switch (index) {
         case WIV_CMS_TRC_GAMMA:
-            return float4(gamma(color.rgb), color.a);
+            return float4(gamma_to_linear(color.rgb), color.a);
         case WIV_CMS_TRC_SRGB:
-            return float4(srgb(color.rgb), color.a);
+            return float4(srgb_to_linear(color.rgb), color.a);
         
         // Red image.
         default:
