e2ff29473e34b69b531588b1dc6369d9cb6ea102
garamond13 <98652255+garamond13@users.noreply.github.com>
2025-05-07 18:52:47 +0200

    Update version number

--- w-image-viewer/version.h
+++ w-image-viewer/version.h
@@ -1,5 +1,5 @@
 #pragma once
 
 inline constexpr int WIV_VERSION_NUMBER_MAJOR = 1;
-inline constexpr int WIV_VERSION_NUMBER_MINOR = 7;
+inline constexpr int WIV_VERSION_NUMBER_MINOR = 8;
 inline constexpr int WIV_VERSION_NUMBER_PATCH = 0;
